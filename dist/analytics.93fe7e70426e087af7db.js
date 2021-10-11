e6d93f79297644de43012290292f8e5d344f71f6
Elijah <elijahmylcev@gmail.com>
2021-10-11 22:17:22 +0300

    Added style&css -loader

--- dist/analytics.93fe7e70426e087af7db.js
+++
@@ -1,1 +0,0 @@
-window.analytics=function(){let e=0,t=!1;const n=()=>e++;return document.addEventListener("click",n),{destroy(){document.removeEventListener("click",n),t=!0},getClicks:()=>t?`Analytics is destroyed. Clics = ${e}`:e}}();
